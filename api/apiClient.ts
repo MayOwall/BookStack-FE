b7f98f9a951cc9b5890c6dadf4460b7bc467e01f
Owall <97934878+MayOwall@users.noreply.github.com>
2023-05-04 18:42:20 +0900

    fix : BaseURL 수정

--- api/apiClient.ts
+++ api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://bookstack-be.fly.dev";
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://bookstack-be.fly.dev";
+// const BASE_URL = "http://localhost:8080";
 
 // instance axios
 const apiClient = axios.create({
